db3ef860259cef01ea2eac6d7279b3f09e872509
Kyle Anderson <kwa@yelp.com>
2015-02-05 12:53:14 -0800

    Added a timeout_and_retry block for pagerduty

--- files/pagerduty.rb
+++ files/pagerduty.rb
@@ -11,24 +11,45 @@ class Pagerduty < BaseHandler
     team_data('pagerduty_api_key') || false
   end
 
+  def timeout_and_retry(&block)
+    response = false
+    3.times do
+      begin
+        timeout(10) do
+          response = block
+          return true if response == 'success'
+        end
+      rescue Timeout::Error
+      end
+      sleep 3
+    end
+    response
+  end
+
   def trigger_incident
     return false unless api_key
     require 'redphone/pagerduty'
-    Redphone::Pagerduty.trigger_incident(
-      :service_key  => api_key,
-      :incident_key => incident_key,
-      :description  => description,
-      :details      => full_description_hash
-    )['status'] == 'success'
+    status = timeout_and_retry do
+      Redphone::Pagerduty.trigger_incident(
+        :service_key  => api_key,
+        :incident_key => incident_key,
+        :description  => description,
+        :details      => full_description_hash
+      )['status']
+    end
+    status
   end
 
   def resolve_incident
     return false unless api_key
     require 'redphone/pagerduty'
-    Redphone::Pagerduty.resolve_incident(
-      :service_key  => api_key,
-      :incident_key => incident_key
-    )['status'] == 'success'
+    status = timeout_and_retry do
+      status = Redphone::Pagerduty.resolve_incident(
+        :service_key  => api_key,
+        :incident_key => incident_key
+      )['status']
+    end
+    status
   end
 
   def handle
@@ -36,26 +57,21 @@ class Pagerduty < BaseHandler
       puts "pagerduty -- Ignoring incident #{incident_key} as it is not set to page."
       return
     end
-    begin
-      timeout(10) do
-        action = 'nil'
-        response = case @event['check']['status'].to_i
-        when 2
-          action = 'trigger'
-          trigger_incident
-        when 0,1
-          action = 'resolve'
-          resolve_incident
-        end
-        if response
-          puts 'pagerduty -- ' + action.capitalize + 'd incident -- ' + incident_key
-        else
-          puts 'pagerduty -- failed to ' + action + ' incident -- ' + incident_key
-        end
-      end
-    rescue Timeout::Error
-      puts 'pagerduty -- timed out while attempting to ' + @event['action'] + ' a incident -- ' + incident_key
+    action = 'nil'
+    response = case @event['check']['status'].to_i
+    when 2
+      action = 'trigger'
+      trigger_incident
+    when 0,1
+      action = 'resolve'
+      resolve_incident
+    end
+    if response
+      puts 'pagerduty -- ' + action.capitalize + 'd incident -- ' + incident_key
+    else
+      puts 'pagerduty -- failed to ' + action + ' incident -- ' + incident_key
     end
   end
+
 end
 
