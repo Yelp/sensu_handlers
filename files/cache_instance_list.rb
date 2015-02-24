f34b9374f9bd08cf8672e3540c04385390c7c210
Dmitriy Samovskiy <dmits@yelp.com>
2015-02-24 10:30:58 -0800

    fog credential file in cache_instance_list

--- files/cache_instance_list.rb
+++ files/cache_instance_list.rb
@@ -4,6 +4,7 @@ require 'fog'
 require 'json/pure'
 require 'sensu-plugin/check/cli'
 require 'fileutils'
+require 'yaml'
 
 class Instance_list < Sensu::Plugin::Check::CLI
 
@@ -11,19 +12,25 @@ class Instance_list < Sensu::Plugin::Check::CLI
     :short => '-a AWS_ACCESS_KEY',
     :long => '--aws-access-key AWS_ACCESS_KEY',
     :description => "AWS Access Key. Either set ENV['AWS_ACCESS_KEY_ID'] or provide it as an option",
-    :required => true
+    :required => false
 
   option :aws_secret_access_key,
     :short => '-k AWS_SECRET_ACCESS_KEY',
     :long => '--aws-secret-access-key AWS_SECRET_ACCESS_KEY',
     :description => "AWS Secret Access Key. Either set ENV['AWS_SECRET_ACCESS_KEY'] or provide it as an option",
-    :required => true
+    :required => false
 
   option :aws_region,
     :short => '-r AWS_REGION',
     :long => '--aws-region REGION',
     :description => "AWS Region (such as eu-west-1).",
     :default => 'us-east-1'
+
+  option :credential_file,
+    :short => '-f credential_file',
+    :long => '--credential-file credential_file',
+    :description => 'fog credential file',
+    :required => false
 
   def write_instance_cache(instance_list)
     bail if instance_list.nil?
@@ -33,12 +40,14 @@ class Instance_list < Sensu::Plugin::Check::CLI
 
   def ec2
     @ec2 ||= begin
-      Fog::Compute.new({
-        :provider => 'AWS',
-        :aws_access_key_id      => config[:aws_access_key],
-        :aws_secret_access_key  => config[:aws_secret_access_key],
-        :region                 => config[:aws_region]
-      })
+      opts = { :provider => 'AWS', :region => config[:aws_region] }
+      if config[:credential_file]
+        Fog.credentials_path = config[:credential_file]
+      elsif config[:aws_access_key] && config[:aws_secret_access_key]
+        opts.merge! :aws_access_key_id      => config[:aws_access_key],
+                    :aws_secret_access_key  => config[:aws_secret_access_key]
+      end
+      Fog::Compute.new opts
     end
   end
 
