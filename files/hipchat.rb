6bf3987d20134adab6b1d0d13743b466ede53178
Drew Wilson <dwilson@opentable.com>
2015-09-02 16:36:21 +0100

    add incident and handle methods and spec tests for hipchat handler

--- files/hipchat.rb
+++ files/hipchat.rb
@@ -2,4 +2,55 @@ require "#{File.dirname(__FILE__)}/base"
 #require_relative 'base'
 
 class Hipchat < BaseHandler
+  def handle
+    response = timeout_and_retry do
+      case @event['check']['status'].to_i
+      when 1,2
+        trigger_incident
+      when 0
+        resolve_incident
+      end
+    end
+  end
+
+  def api_key
+    settings[self.class.name.downcase]['apikey'] || false
+  end
+
+  def trigger_incident
+    alert_hipchat(color: hipchat_message_colour, notify: true)
+  end
+
+  def resolve_incident
+    alert_hipchat(color: hipchat_message_colour)
+  end
+
+  def hipchat_message
+"
+<b>#{Time.at(@event['check']['issued'])} - #{@event['client']['name'] + '/' + @event['check']['name']} on #{@event['client']['name']} (#{@event['client']['address']}) - #{human_check_status}</b>
+<br /><br />
+&nbsp;&nbsp;#{@event['check']['notification'] || @event['check']['output']}
+"
+  end
+
+  def hipchat_message_colour
+    case @event['check']['status']
+    when 0
+      'green'
+    when 1
+      'yellow'
+    when 2
+      'red'
+    else
+      'grey'
+    end
+  end
+
+  def alert_hipchat(options_or_notify = {})
+    return false unless api_key
+    return true
+
+    # hipchat_client = HipChat::Client.new(api_key)
+    # hipchat_client[room].send('sensu', hipchat_message, options_or_notify)
+  end
 end
--- spec/functions/hipchat_spec.rb
+++ spec/functions/hipchat_spec.rb
@@ -23,4 +23,130 @@ describe Hipchat do
   # two silly tests just for starter
   it { expect(subject).to be_a BaseHandler }
   it { expect(subject).to be_a Sensu::Handler }
+
+  describe 'trigger_incident' do
+    it 'returns false when no hipchat api_key' do
+      expect(subject.trigger_incident).to be false
+    end
+
+    it 'returns true when hipchat api_key exists' do
+      subject.settings['hipchat']['apikey'] = 'fakekey'
+      expect(subject.trigger_incident).to be true
+    end
+  end
+
+  describe 'resolve_incident' do
+    it 'returns false when no hipchat api_key' do
+      expect(subject.resolve_incident).to be false
+    end
+
+    it 'returns true when hipchat api_key exists' do
+      subject.settings['hipchat']['apikey'] = 'fakekey'
+      expect(subject.resolve_incident).to be true
+    end
+  end
+
+  describe 'handle' do
+    context 'when check status is 0' do
+      before do
+        subject.event['check']['status'] = 0
+      end
+
+      context 'when resolve_incident returns true' do
+        it 'calls resolve_incident once' do
+          expect(subject).to receive(:resolve_incident)
+            .once
+            .and_return(true)
+
+          subject.handle
+        end
+
+        it 'calls alert_hipchat with options color green' do
+          expect(subject).to receive(:alert_hipchat)
+            .with( { :color => 'green' } )
+            .and_return(true)
+
+          subject.handle
+        end
+      end
+
+      context 'when resolve_incident returns false' do
+        it 'calls resolve_incident 3 times' do
+          expect(subject).to receive(:resolve_incident)
+            .exactly(3).times
+            .and_return(false)
+
+          subject.handle
+        end
+      end
+    end
+
+    context 'when check status is 1' do
+      before do
+        subject.event['check']['status'] = 1
+      end
+
+      context 'when trigger_incident returns true' do
+        it 'calls trigger_incident once' do
+          expect(subject).to receive(:trigger_incident)
+            .once
+            .and_return(true)
+
+          subject.handle
+        end
+      end
+
+      context 'when trigger_incident returns false' do
+        it 'calls trigger_incident 3 times' do
+          expect(subject).to receive(:trigger_incident)
+            .exactly(3).times
+            .and_return(false)
+
+          subject.handle
+        end
+      end
+
+      it 'calls alert_hipchat with options color yellow & notify true' do
+        expect(subject).to receive(:alert_hipchat)
+          .with( { :color => 'yellow', :notify => true } )
+          .and_return(true)
+
+        subject.handle
+      end
+    end
+
+    context 'when check status is 2' do
+      before do
+        subject.event['check']['status'] = 2
+      end
+
+      context 'when trigger_incident returns true' do
+        it 'calls trigger_incident once' do
+          expect(subject).to receive(:trigger_incident)
+            .once
+            .and_return(true)
+
+          subject.handle
+        end
+      end
+
+      # context 'when trigger_incident returns false' do
+      #   it 'calls trigger_incident 3 times' do
+      #     expect(subject).to receive(:trigger_incident)
+      #       .exactly(3).times
+      #       .and_return(false)
+      #
+      #     subject.handle
+      #   end
+      # end
+
+      it 'calls alert_hipchat with options color red & notify true' do
+        expect(subject).to receive(:alert_hipchat)
+          .with( { :color => 'red', :notify => true } )
+          .and_return(true)
+
+        subject.handle
+      end
+    end
+  end
 end
