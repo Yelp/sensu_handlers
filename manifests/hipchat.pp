d94431f67db49c272f67769ee9ab077affbf6dfa
fess <fess@fess.org>
2015-09-04 08:13:41 -0700

    hipchat requires api_key

--- manifests/hipchat.pp
+++ manifests/hipchat.pp
@@ -1,13 +1,22 @@
 # == Class: sensu_handlers::hipchat
 #
 # Sensu handler for sending to hipchat using hipchat gem
-class sensu_handlers::hipchat inherits sensu_handlers {
+#
+# == Parameters
+#
+# [*api_key*]
+#
+#  hipchat api key
+class sensu_handlers::hipchat (
+  $api_key
+) inherits sensu_handlers {
 
  sensu::handler { 'hipchat':
     type    => 'pipe',
     source  => 'puppet:///modules/sensu_handlers/hipchat.rb',
     config  => {
-      teams => $teams,
+      api_key => $api_key,
+      teams   => $teams,
     }
   }
 
