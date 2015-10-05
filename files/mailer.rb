5a919599bbdf40098ffa08ef75eee9a3f428a5b0
fess <fess@fess.org>
2015-10-05 12:10:03 -0700

    use handler_settings like all the rest

--- files/mailer.rb
+++ files/mailer.rb
@@ -59,7 +59,7 @@ class Mailer < BaseHandler
     # Only procede if we have an email address to work with
     return false unless mail_to
 
-    mail_from = settings['mailer']['mail_from'] || 'sensu@yelp.com'
+    mail_from = handler_settings['mail_from'] || 'sensu@yelp.com'
 
     delivery_method = handler_settings['delivery_method'] || 'smtp'
     smtp_address = handler_settings['smtp_address'] || 'localhost'
