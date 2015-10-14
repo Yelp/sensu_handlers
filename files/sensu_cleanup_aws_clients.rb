eec5e4f7aa6af76fbde6f25666af9f7c866d64ca
Alexander Dudko <adudko@yelp.com>
2015-10-14 14:36:12 -0700

    Added logging

--- files/sensu_cleanup_aws_clients.rb
+++ files/sensu_cleanup_aws_clients.rb
@@ -1,26 +1,52 @@
 #!/opt/puppet-omnibus/embedded/bin/ruby
 
+# Delete all terminated or missing AWS clients from sensu
+#
+# 1. Get Sensu API creds from /etc/sensu/conf.d/api.json
+# 2. Get Sensu clients that have a valid instance_id
+# 3. Get AWS instance_id list from /var/cache/instance_list.json
+# 4. Find a diff between two lists
+# 5. Delete all sensu clients listed in diff
+#
+# TODO: prevent from deletion all sensu clients when 
+# instance_list.json is (partially?) empty
+
+require 'logger'
 require 'net/http'
 require 'json'
 
+PATH_SENSU_API_JSON = '/etc/sensu/conf.d/api.json'
+PATH_AWS_INSTANCE_LIST_JSON = '/var/cache/instance_list.json'
+
+$logger = Logger.new(STDOUT)
+$logger.level = Logger::DEBUG
+
+
 def load_instances_cache
-  f = File.open('/var/cache/instance_list.json')
+  $logger.debug("Loading AWS data from cache (#{PATH_AWS_INSTANCE_LIST_JSON})")
+  f = File.open(PATH_AWS_INSTANCE_LIST_JSON)
   instances = JSON.load(f)
   bail if instances.nil?
   id_hash = Hash.new
   instances.each { |r| id_hash[r['id']] = { 'state' => r['state'], 'tags' => r['tags']} }
+  $logger.debug("Loaded #{id_hash.keys.count} instances.")
   id_hash
 end
 
 def sensu_api_creds
-  f = File.open('/etc/sensu/conf.d/api.json')
+  $logger.debug("Loading Sensu API creds from #{PATH_SENSU_API_JSON}")
+  f = File.open(PATH_SENSU_API_JSON)
   c = JSON.load(f)
   c = c['api']
   { 'host' => c['host'], 'port' => c['port'], 
     'user' => c['user'], 'pass' => c['password'] }
 end
 
+#def send_http_request(host, port, request)
+#end
+
 def sensu_get_clients_with_instance_id(creds)
+  $logger.debug('Retrieving clients list from Sensu API.')
   uri = URI("http://#{creds[:host]}/clients")
   response = nil
   Net::HTTP.start(creds['host'], creds['port']) do |http|
@@ -36,6 +62,8 @@ def sensu_get_clients_with_instance_id(creds)
       id_hash[c['instance_id']] = c['name']
     end
   end
+
+  $logger.debug("Got #{id_hash.keys.count} (AWS) out of #{json.count} clients.")
   id_hash
 end
 
@@ -47,16 +75,27 @@ def sensu_delete_client(creds, client)
     request.basic_auth creds['user'], creds['pass']
     response = http.request request # Net::HTTPResponse object
   end
-  puts response.code, response.body
+
+  case response.code
+  when '202'
+      $logger.info("EC2 Node - [202] Successfully deleted Sensu client: #{client}")
+  when '404'
+      $logger.error("EC2 Node - [404] Unable to delete #{client}, doesn't exist!")
+  when '500'
+      $logger.error("EC2 Node - [500] Miscellaneous error when deleting #{client}")
+  else
+      $logger.error("EC2 Node - [#{response.code}] Completely unsure of what happened!")
+  end
 end
 
 def main
   sensu_creds = sensu_api_creds
 
   sensu_clients = sensu_get_clients_with_instance_id(sensu_creds)
-  awsinstances = load_instances_cache
-
+  aws_instances = load_instances_cache
+  
   diff = sensu_clients.keys - aws_instances.keys
+  $logger.info("#{diff.count} Sunsu clients to delete.")
   diff.each { |h| sensu_delete_client(sensu_creds, sensu_clients[h]) }
 end
 
