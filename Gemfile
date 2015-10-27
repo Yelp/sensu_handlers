dcd463200ce5c102badc505df016d7c5b0cee859
Alexander Dudko <adudko@yelp.com>
2015-10-27 09:43:16 -0700

    sensu_cleanup_aws_clients works direcly with AWS API

--- Gemfile
+++ Gemfile
@@ -20,4 +20,4 @@ gem 'hiera-puppet-helper',
   :ref => '5ed989a130bc62cc6bdb923596586284f0bd73df'
 
 gem 'mail', '~> 2.5.4'
-
+gem 'aws-sdk', '~> 2'
--- files/sensu_cleanup_aws_clients.rb
+++ files/sensu_cleanup_aws_clients.rb
@@ -1,82 +1,90 @@
-#!/opt/puppet-omnibus/embedded/bin/ruby
+#!/usr/bin/env ruby
 
 # Delete all terminated or missing AWS clients from sensu
 #
 # 1. Get Sensu API creds from /etc/sensu/conf.d/api.json
 # 2. Get Sensu clients that have a valid instance_id
-# 3. Get AWS instance_id list from /var/cache/instance_list.json
+# 3. Get AWS instance_id list
 # 4. Find a diff between two lists
 # 5. Delete all sensu clients listed in diff
 #
 # TODO: prevent from deletion all sensu clients when 
 # instance_list.json is (partially?) empty
 
+require 'optparse'
+require 'ostruct'
 require 'logger'
 require 'net/http'
 require 'json'
+require 'aws-sdk'
+require 'aws-sdk-resources'
 
 PATH_SENSU_API_JSON = '/etc/sensu/conf.d/api.json'
-PATH_AWS_INSTANCE_LIST_JSON = '/var/cache/instance_list.json'
+PATH_AWS_API_JSON = '/etc/sensu/cache_instance_list_creds.yaml'
 
-class SensuCleanupAwsClients
+class SensuApiConnector
 
-  def initialize
-    @logger = Logger.new(STDOUT)
-    @logger.level = Logger::DEBUG
-    @sensu_creds = sensu_api_creds
-  end
+  def initialize(host, port, user, pass, logger=nil)
+    @sensu_host = host
+    @sensu_port = port
+    @sensu_user = user
+    @sensu_pass = pass
+    if logger.nil?
+      @logger = Logger.new(STDOUT)
+      @logger.level = Logger::ERROR
+    else
+      @logger = logger
+    end
 
-  def load_instances_cache
-    @logger.debug("Loading AWS data from cache (#{PATH_AWS_INSTANCE_LIST_JSON})")
-    f = File.open(PATH_AWS_INSTANCE_LIST_JSON)
-    instances = JSON.load(f)
-    bail if instances.nil?
-    id_hash = Hash.new
-    instances.each { |r| id_hash[r['id']] = { 'state' => r['state'], 'tags' => r['tags']} }
-    @logger.debug("Loaded #{id_hash.keys.count} instances.")
-    id_hash
-  end
-
-  def sensu_api_creds
-    #@logger.debug("Loading Sensu API creds from #{PATH_SENSU_API_JSON}")
-    f = File.open(PATH_SENSU_API_JSON)
-    c = JSON.load(f)
-    c = c['api']
-    { 'host' => c['host'], 'port' => c['port'], 
-      'user' => c['user'], 'pass' => c['password'] }
+    @sensu_http = Net::HTTP.new(@sensu_host, @sensu_port)
+    @sensu_http.open_timeout = 2
   end
 
   def send_http_request(request)
+    request.basic_auth @sensu_user, @sensu_pass
     response = nil
-    Net::HTTP.start(@sensu_creds['host'], @sensu_creds['port']) do |http|
-      request.basic_auth @sensu_creds['user'], @sensu_creds['pass']
-      response = http.request request # Net::HTTPResponse object
+    begin
+      response = @sensu_http.request request # Net::HTTPResponse object
+    rescue Net::OpenTimeout => e
+      @logger.fatal("Can't connect to Sensu API (#{request.uri})")
     end
     response
   end
 
-  def sensu_get_clients_with_instance_id
+  def get_clients_with_instance_id
     @logger.debug('Retrieving clients list from Sensu API.')
-    request = Net::HTTP::Get.new URI("http://#{@sensu_creds[:host]}/clients")
+    request = Net::HTTP::Get.new URI("http://#{@sensu_host}/clients")
     response = send_http_request(request)
 
-    json = JSON.parse(response.body)
-    id_hash = Hash.new
-    json.each do |c| 
-      if !c['instance_id'].nil?
-        id_hash[c['instance_id']] = c['name']
+    if !response.nil?
+      if response.code == '200'
+        json = JSON.parse(response.body)
+
+        id_hash = json.inject({}) { 
+          |h, node| h[node['instance_id']] = \
+          node['name'] if !node['instance_id'].nil?; h }
+
+        @logger.debug("Got #{id_hash.keys.count} (AWS) out of #{json.count} Sensu clients.")
+        return id_hash
+      else
+        @logger.fatal("Unexpected response from Sensu API (code: #{response.code}, message: #{response.message})")
       end
     end
 
-    @logger.debug("Got #{id_hash.keys.count} (AWS) out of #{json.count} clients.")
-    id_hash
+    return nil
   end
 
-  def sensu_delete_client(client)
-    request = Net::HTTP::Get.new URI("http://#{@sensu_creds[:host]}/clients/#{client}") # TODO switch from Get to Delete
+  def delete_client(client)
+    request = Net::HTTP::Delete.new URI("http://#{@sensu_host}/clients/#{client}")
     response = send_http_request(request)
 
-    case response.code
+    if !response.nil?
+      log_delete_result(client, response.code)
+    end
+  end
+
+  def log_delete_result(client, code)
+    case code
     when '202'
         @logger.info("EC2 Node - [202] Successfully deleted Sensu client: #{client}")
     when '404'
@@ -88,17 +96,126 @@ class SensuCleanupAwsClients
     end
   end
 
+end
+
+
+class AwsApiConnector
+
+  def initialize(region, aws_access_key_id, aws_secret_access_key, logger=nil)
+    if logger.nil?
+      @logger = Logger.new(STDOUT)
+      @logger.level = Logger::ERROR
+    else
+      @logger = logger
+    end
+
+    Aws.config.update({
+      region: region,
+      credentials: Aws::Credentials.new(aws_access_key_id, aws_secret_access_key),
+    })
+  end
+
+  def get_ec2_instanses_info
+    @logger.debug('Retrieving list of EC2 instances from AWS API.')
+    ec2 = Aws::EC2::Resource.new
+    id_hash = ec2.instances().inject({}) \
+      { |m, i| m[i.id] = { 'state' => i.state['name'], 'tags' => i.tags }; m }
+    @logger.debug("Got #{id_hash.keys.count} AWS instances.")
+    id_hash
+  end
+
+end
+
+
+class SensuCleanupAwsClients
+
+  def initialize(aws_region, log_level, noop=false)
+    @logger = Logger.new(STDOUT)
+    @logger.level = log_level
+    @noop = noop
+
+    sc = sensu_api_creds
+    @sensu = SensuApiConnector.new(
+      sc['host'], sc['port'], sc['user'], sc['pass'], @logger)
+
+    ac = read_aws_creds_from_yaml(PATH_AWS_API_JSON)
+    @aws = AwsApiConnector.new(
+      aws_region, ac['aws_access_key_id'], ac['aws_secret_access_key'], @logger)
+  end
+
+  def read_aws_creds_from_yaml(creds_yaml)
+    @logger.debug("Retrieving AWS API creds from #{creds_yaml}")
+    YAML.load_file(creds_yaml)['default']
+  end
+
+  def sensu_api_creds
+    @logger.debug("Retrieving Sensu connection info from #{PATH_SENSU_API_JSON}")
+    f = File.open(PATH_SENSU_API_JSON)
+    c = JSON.load(f)
+    c = c['api']
+    { 'host' => c['host'], 'port' => c['port'], 
+      'user' => c['user'], 'pass' => c['password'] }
+  end
+
   def main
-    sensu_clients = sensu_get_clients_with_instance_id
-    aws_instances = load_instances_cache
-  
+    sensu_clients = @sensu.get_clients_with_instance_id
+    if sensu_clients.nil?
+      return
+    end
+
+    aws_instances = @aws.get_ec2_instanses_info.reject { |id, val| val['state'] == 'terminated' }
+    @logger.debug("Found #{aws_instances.keys.count} non-terminated AWS instances.")
+
     diff = sensu_clients.keys - aws_instances.keys
     @logger.info("#{diff.count} Sunsu clients to delete.")
-    diff.each { |h| sensu_delete_client(sensu_clients[h]) }
+
+    if !(sensu_clients.keys.count == diff.count and diff.count > 1)
+      diff.each { |h| sensu_delete_client(sensu_clients[h]) } if !@noop
+    else
+      @logger.warn("Rejecting to delete all Sensu clients.")
+    end
   end
 
 end # SensuCleanupAwsClients
 
-job = SensuCleanupAwsClients.new
-job.main
+if __FILE__ == $0
+
+  options = OpenStruct.new
+  options.verbose = false
+  options.noop = false
+  options.region = ''
+
+  optparse = OptionParser.new do |opts|
+    opts.banner = "Usage: #{$0} [options]"
+
+    opts.on("-v", "--verbose", "Run verbosely") do |v|
+      options.verbose = v
+    end
+
+    opts.on("-n", "--noop", "Do not delete sensu clients.") do |n|
+      options.noop = n
+    end
+
+    opts.on("-r", "--region AWS_REGION", "AWS region to query.") do |r|
+      options.region << r
+    end
+  end
+
+  begin
+    optparse.parse!(ARGV)
+  rescue OptionParser::InvalidOption, OptionParser::MissingArgument => e
+    puts e, optparse.help
+    exit(1)
+  end
+
+  if options.region.empty?
+    puts 'AWS_REGION must be set', optparse.help
+    exit(1)
+  end
+
+  log_level = options.verbose ? Logger::DEBUG : Logger::INFO
+
+  job = SensuCleanupAwsClients.new(options.region, log_level, options.noop)
+  job.main()
+end
 
