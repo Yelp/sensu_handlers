e9b928554a1fa226af11f2b1a7265bde83ebd33e
Kyle Anderson <kwa@yelp.com>
2016-03-04 16:30:53 -0800

    Reduce duplicate pages by using region instead of habitat in the PD key

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,7 +28,7 @@ module SensuHandlerTestHelper
     event['check']['status'] ||= 0
     event['check']['output'] ||= 'some check output'
     event['check']['issued'] ||= Time.now()
-    event['check']['habitat'] = 'somehabitat'
+    event['check']['region'] = 'someregion'
     subject.event = event
     subject.settings          = Hash.new
     subject.settings['default']  = Hash.new
