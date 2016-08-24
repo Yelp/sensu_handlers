6c380b379e6ff4201f4608d498e4e11c14aa14b2
Kyle Anderson <kwa@yelp.com>
2016-08-24 09:42:20 -0700

    Remove last remnants of cron::d. Fixes #44

--- spec/fixtures/manifests/site.pp
+++ spec/fixtures/manifests/site.pp
@@ -5,22 +5,3 @@ include apt
 include sensu
 
 package { 'rubygem-sensu-plugin': }
-
-define cron::d (
-  $minute,
-  $command,
-  $user,
-  $second='0',
-  $hour='*',
-  $dom='*',
-  $month='*',
-  $dow='*',
-  $mailto='""',
-  $log_to_syslog=true,
-  $staleness_threshold=undef,
-  $staleness_check_params=undef,
-  $annotation='',
-  $lock=false,
-  $normalize_path=hiera('cron::d::normalize_path', false),
-  $comment='',
-) { }
