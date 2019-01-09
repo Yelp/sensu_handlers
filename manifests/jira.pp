19578b9c1afe8f2edf8f597f5285af852a148c8c
Jason Perrin <jvperrin@yelp.com>
2019-01-09 15:49:07 -0800

    Remove old lucid patch

--- manifests/jira.pp
+++ manifests/jira.pp
@@ -35,17 +35,4 @@ class sensu_handlers::jira (
       $sensu_handlers::num_occurrences_filter,
     ]),
   }
-  if $::lsbdistcodename == 'Lucid' {
-    # So sorry for the httprb monkeypatch. It is Debian bug 564168 that took
-    # me forever to track down. Maybe someday we'll use a newer ruby.
-    # Afterall, who supports versions that are EOL?
-    # https://www.ruby-lang.org/en/news/2013/06/30/we-retire-1-8-7/
-    # What are they going to deprecate next? ifconfig?
-    file_line { 'fix_httprb_564168':
-      match => '      @socket.close unless.*',
-      line  => '      @socket.close unless @socket.nil? || @socket.closed?',
-      path  => '/usr/lib/ruby/1.8/net/http.rb',
-    }
-  }
-
 }
